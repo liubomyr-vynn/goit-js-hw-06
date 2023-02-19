c86b8dec56c2b31024bb0b23ec990f8deea5749e
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-02-19 13:24:08 +0200

    Task 01-js

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const itemEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemEl.length}`);
+itemEl.forEach(element => {
+	console.log(`Category: ${element.firstElementChild.textContent}`);
+	console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
