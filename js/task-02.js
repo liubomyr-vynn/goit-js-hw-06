79b73bf04b82decdb0c9515af4854411f72f543b
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-02-19 13:24:42 +0200

    Task 02-js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const ingredientEl = ingredients.map(ingredient => {
+	const itemEl = document.createElement("li");
+	itemEl.textContent = ingredient;
+	itemEl.classList.add("item");
+	return itemEl;
+});
+list.append(...ingredientEl);
