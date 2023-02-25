0907af2989b446eebaa696306561777094fb7ff9
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-02-25 21:55:27 +0200

    Update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+const handleChangeColor = () => {
+	colorEl.innerHTML = getRandomHexColor();
+	bodyEl.style.backgroundColor = colorEl.innerHTML;
+};
+buttonEl.addEventListener("click", handleChangeColor);
