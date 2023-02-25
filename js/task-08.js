ad13f160e25bccf67aefbef5bf395ec60501b26b
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-02-25 21:55:17 +0200

    Update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginFormEl = document.querySelector(".login-form");
 
+const handleSubmit = event => {
+	event.preventDefault();
+
+	const formElements = event.currentTarget.elements;
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+	if (email === "" || password === "") {
+		return alert("Заповніть всі поля!");
+	}
+
+	const formData = {
+		email,
+		password,
+	};
+
+	console.log(formData);
+	event.currentTarget.reset();
+};
+
+loginFormEl.addEventListener("submit", handleSubmit);
